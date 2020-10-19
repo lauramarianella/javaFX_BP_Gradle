6d69231a8bf6727e1b95ad73e4d05fb006bb0b51
lauramarianella <lauramarianella@gmail.com>
2020-10-19 18:19:09 -0400

    Adding FXML example

--- src/main/java/MyPkg/CH_31/JavaFX_FXML/FXMLController.java
+++ src/main/java/MyPkg/CH_31/JavaFX_FXML/FXMLController.java
@@ -1,8 +1,13 @@
 package MyPkg.CH_31.JavaFX_FXML;
 
-public class FXMLController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class FXMLController {
+    @FXML private Label lblText;
+    
     public void handleOnClick(){
         System.out.println("You clicked me!!");
+        lblText.setText("You clicked me!!");
     }
 }
